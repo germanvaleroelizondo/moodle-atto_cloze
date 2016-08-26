d7b7b2109acae09c5c252763af16cce557e0882e
Daniel Thies <dthies@ccal.edu>
2016-08-26 12:46:31 -0500

    Delete delegate

--- yui/build/moodle-atto_cloze-button/moodle-atto_cloze-button-debug.js
+++ yui/build/moodle-atto_cloze-button/moodle-atto_cloze-button-debug.js
@@ -282,7 +282,6 @@ Y.namespace('M.atto_cloze').Button = Y.Base.create('button', Y.M.editor_atto.Edi
                 }));
             this._form = content;
 
-            content.delegate('click', this._choiceHandler, '.' + CSS.SUBMIT + ', .' + CSS.DUPLICATE, this);
             content.one('.' + CSS.SUBMIT).on('click', this._choiceHandler, this);
             content.one('.' + CSS.CANCEL).on('click', this._cancel, this);
             return content;
